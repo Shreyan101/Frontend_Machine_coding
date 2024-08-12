e56dbaf691a456dfcfc44cc55f9acbdc430f3807
shreyan kashyap <shreyankashyap101@gmail.com>
2024-08-12 21:52:01 +0530

    Added Question for DataTable

--- src/Pages/DataTable/DataTable.js
+++ src/Pages/DataTable/DataTable.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './DataTable.css';
 
-const DataTable = () => {
-  return <div>DataTable</div>;
-};
+function DataTable() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      let data = await (
+        await fetch('https://jsonplaceholder.typicode.com/users')
+      ).json();
+      setData(data);
+    } catch (e) {
+      console.log('Error fetching data:', e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className='data-table-container'>
+      <h2>User Data Table</h2>
+      <table className='data-table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default DataTable;
